8e6991892a2b476f0885ece161bb868f14a6dcb4
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-02 20:54:53 +0530

    player component

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Body from "./Body";
+import Footer from "./Footer";
 import "./Player.css";
-function Player() {
+import Sidebar from "./Sidebar";
+
+function Player({ spotify }) {
   return (
-    <div>
-      <h1>Welcome to spotify</h1>
+    <div className="player">
+      <div className="player_body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      <Footer />
     </div>
   );
 }
