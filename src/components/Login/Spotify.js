b8e9bfc2806983dab70cbd38f343f30612e029db
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-02 14:32:34 +0530

    comments

--- src/components/Login/Spotify.js
+++ src/components/Login/Spotify.js
@@ -1,8 +1,14 @@
-export const authEndpoint = "https://accounts.spotify.com/authorize"; //This is what spotify does it take care of authentication and playlist of different users. (clicking login page it redirects to spotify authorization )
+// Spotify takes care of user authentication part on its own.
+// (clicking login page it redirects to spotify authorization )
+export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/"; // And this is the redirect url we gave in spotify developers, To redirect back to our localhost where app is running. (ones authorozed take back to localhost)
-const clientId = "bc7a759ec9424c8a94be098c7be3b077"; //Client id is got from spotify developers
-//ok this is users scopes. people has read access to songs. like it can be played, view recent played, playback state, modify it.
+// And this is the redirect url developers provide in spotify developers.  To redirect back to our localhost where app is running.
+const redirectUri = "http://localhost:3000/";
+
+//Client id is got from spotify developers.
+const clientId = "bc7a759ec9424c8a94be098c7be3b077";
+
+//Users scopes provide the different read only features of spotify.
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
@@ -10,24 +16,25 @@ const scopes = [
   "user-top-read",
   "user-modify-playback-state",
 ];
-//what all can be done in spotify.
 
 export const getTokenFromUrl = () => {
-  //The response got from token is taken
-  return window.location.hash //returns to the poi nt we had "/", that is localhost
+  return window.location.hash //This returns to the point we had "/", that is localhost
     .substring(1) //chopping string. and make to substring.
     .split("&")
     .reduce((initial, item) => {
-      //reduce in array method, initial value of reduce and item.
       let parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]); //go to initial array, decode the uri and return initial
-
       return initial;
-    }, {}); //this empty object, is how initial should start with.
+    }, {});
+  //this empty object, is how initial should start with.
 };
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
-//This login url will be displayed as url in localhost when we press login  button.
-//loginurl is using the endpoint of spotify, the user is send to that end point. and see if there is a client id, if there is it accepts it and redirect uri it accepts it. and scope also checks. and then join with a aASCII space character. and response type is given to a token, what that is doing is, if everything is ok user got backs a token. and then it gives back a prompt that is dialogue= true. if dialogue false, that pop up when hitting login wont come.
+//This login url will be displayed in the url when we're in authorization page.
+//login url is using the endpoint of spotify, the user is send to that end point.
+// and see if there is a client id, if there is, it accepts it and redirect uri.
+// and scope is also checked. and then join with a ASCII space character. and response type is given to a token.
+// if everything is ok user got backs a token. and then it gives back a prompt that is dialogue = true.
+// if dialogue false, that pop up when hitting login wont come.
